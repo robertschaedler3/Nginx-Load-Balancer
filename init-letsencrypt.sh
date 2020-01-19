7240e5a1af4d3f0bb3eed7990d9581159e5ea0f6
Robert Schaedler III <robertschaedler3@gmail.com>
2020-01-18 20:35:24 -0500

    fix data path to match volumes

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(robertschaedler.com www.robertschaedler.com)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./certbot"
 email="robertschaedler3@gmail.com" 
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
